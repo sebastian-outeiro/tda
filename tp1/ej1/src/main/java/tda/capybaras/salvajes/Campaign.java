e346baf4f02266ac69fdc6c17ddc7b618b47a212
sebastian-outeiro <sebastian.outeiro@despegar.com>
2024-03-30 22:21:27 -0300

    TP 1 - Campaing - First impl

---
+++ tp1/ej1/src/main/java/tda/capybaras/salvajes/Campaign.java
@@ -0,0 +1,45 @@
+package tda.capybaras.salvajes;
+
+import java.util.List;
+
+public class Campaign {
+
+    public Solution solve(List<Influencer> influencers) {
+        List<Influencer> influencersOrdered = influencers.stream()
+                .sorted().toList();
+        return backtrack(new Solution(), influencersOrdered, 0);
+    }
+
+    private Solution backtrack(Solution solution, List<Influencer> influencers, Integer position) {
+        if (position >= influencers.size())
+            return solution;
+        Solution bestSolution = solution;
+        Influencer actualInfluencer = influencers.get(position);
+
+        if (solution.canAddInfluencer(actualInfluencer)) {
+            Solution newSolution = solution.copyAdding(actualInfluencer);
+            if (cost(newSolution, influencers, position) > bestSolution.getMarketPenetration()) {
+                newSolution = backtrack(newSolution, influencers, position + 1);
+                if (newSolution.getMarketPenetration() > bestSolution.getMarketPenetration())
+                    bestSolution = newSolution;
+            }
+        }
+
+        if (cost(solution, influencers, position) > bestSolution.getMarketPenetration()) {
+            Solution newSolution = backtrack(solution, influencers, position + 1);
+            if (newSolution.getMarketPenetration() > bestSolution.getMarketPenetration())
+                bestSolution = newSolution;
+        }
+        return bestSolution;
+    }
+
+    private Integer cost(Solution solution, List<Influencer> influencers, Integer position) {
+        Integer actualPenetration = solution.getMarketPenetration();
+        Integer remainInfluencers = influencers.size() - position - 1;
+        Integer nextPenetration = 0;
+        if (position + 1 <= influencers.size() - 1) {
+            nextPenetration = influencers.get(position + 1).getMarketPenetration();
+        }
+        return actualPenetration + remainInfluencers * nextPenetration;
+    }
+}
---
+++ tp1/ej1/src/main/java/tda/capybaras/salvajes/Influencer.java
@@ -0,0 +1,47 @@
+package tda.capybaras.salvajes;
+
+import java.util.List;
+
+public class Influencer implements Comparable{
+    private String id;
+    private String name;
+    private Integer marketPenetration;
+    private List<String> influencersToDiscard;
+
+    public Influencer(String id, String name, Integer marketPenetration, List<String> influencersToDiscard) {
+        this.id = id;
+        this.name = name;
+        this.marketPenetration = marketPenetration;
+        this.influencersToDiscard = influencersToDiscard;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getMarketPenetration() {
+        return marketPenetration;
+    }
+
+    public List<String> getInfluencersToDiscard() {
+        return influencersToDiscard;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        Influencer compareWith = (Influencer) o;
+        return compareWith.getMarketPenetration().compareTo(this.marketPenetration);
+    }
+
+    @Override
+    public String toString() {
+        return "Influencer{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
