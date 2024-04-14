f9f0814385ba194bf35f6667ba0da46260dcf100
Agustin Cavo <acavo@fi.uba.ar>
2024-04-14 04:30:22 -0300

    ej3 version un silo

---
+++ tp1/ej3-version1 silo/src/main/java/org/example/Region.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Region {
+    private String contenido;
+    private Coordenadas coordenadas;
+
+    public Region(int x, int y) {
+
+        this.contenido = " Vacio";
+        this.coordenadas=new Coordenadas(x,y);
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+}
+
